17df1bd117b700f3000c777fa7da19e39dbb0ad9
Thet Hnin Aye <thaye.thethnin@gmail.com>
2018-07-30 09:56:09 +0800

    Trying to commit Task 5

--- DebuggingExercises/DebugOne3.java
+++
@@ -1,9 +0,0 @@
-public class DebugOne3
-{
-   public static void main(String[] args)
-   {
-      System.out.println("Over the river");
-      System.out.println("and through the woods");
-      System.out.println("to Grandmother's house we go");
-   }
-}
